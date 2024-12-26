cd201cedee5f04de311fd4378d0f4e2ec02d647b
lsiwh37249 <lsiwh37249@gmail.com>
2024-12-26 19:39:07 +0900

    [ADD] dag 수정

--- utils/place/place_auto.py
+++ utils/place/place_auto.py
@@ -2,9 +2,12 @@ import subprocess
 
 def run_place_api():
     # 실행할 Python 파일 경로
-    python_file = "utils/place/place_API.py"
+    python_file = "/home/ubuntu/airflow/dags/utils/place/place_API.py"
 
     # 10번 반복 실행
-    for _ in range(300):
+    for _ in range(50):
         subprocess.run(["python", python_file])
 
+
+if __name__ == "__main__":
+    run_place_api()
