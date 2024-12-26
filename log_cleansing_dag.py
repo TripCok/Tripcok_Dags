a596ae44d0f5f0e8b65dfac50506794c83520149
Jeonghoon2 <ukidd12@naver.com>
2024-12-26 17:26:00 +0900

    [FIX] bash_command 수정

--- log_cleansing_dag.py
+++ log_cleansing_dag.py
@@ -22,7 +22,13 @@ with DAG(
     )
 
     spark_host = V.get('spark_host', 'localhost')
-    log_cleansing_command = f"""sudo ssh -i ~/.ssh/spark_key.pem ubuntu@{spark_host} /home/ubuntu/spark/bin/spark-submit /home/ubuntu/etl/py/common/LogsCleansing.py --bucket tripcok --folder topics/tripcok --date 2024-12-25"""
+    log_cleansing_command = f"""sudo ssh -i ~/.ssh/spark_key.pem ubuntu@{spark_host} \
+                                '
+                                bash /home/ubuntu/etl/py/common/environ.sh \ 
+                                /home/ubuntu/spark/bin/spark-submit \
+                                /home/ubuntu/etl/py/common/LogsCleansing.py --bucket tripcok --folder topics/tripcok --date 2024-12-25
+                                '
+"""
 
     log_cleansing_task = BashOperator(
         task_id='log_cleansing_task',
